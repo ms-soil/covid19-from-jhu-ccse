ef1e272368cc8e4698b2c4f81ceffb64f2ff4eca
ms-soil <mschmidh@gwdg.de>
2020-03-14 13:34:49 +0100

    update Mar 13

--- get-data.R
+++ get-data.R
@@ -50,7 +50,10 @@ d <- read_csv("https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/
                                            `3/8/20` = sum(`3/8/20`),
                                            `3/9/20` = sum(`3/9/20`),
                                            `3/10/20` = sum(`3/10/20`),
-                                           `3/11/20` = sum(`3/11/20`))
+                                           `3/11/20` = sum(`3/11/20`),
+                                           `3/12/20` = sum(`3/12/20`),
+                                           `3/13/20` = sum(`3/13/20`))
+
 
 d$`Country/Region`
 # add new dates in list above when available
--- script.R
+++ script.R
@@ -52,14 +52,14 @@ for (i in list (1,2,3,4,5)) {
     theme_bw() +
     ggtitle("Corona cases (data from JHU CCSE)") +
     xlab("Date") +
-    xlim(c(startdate+10, enddate+10)) +
+    xlim(c(startdate+30, enddate+7)) +
     ylab("Confirmed cases") +
     geom_line(aes(c1$date_readable, c1$cases),col = "red", size = 1) +
     geom_point(aes(c1$date_readable, c1$cases),col = "black", size = 2) +
-    geom_label_repel(aes(c1$date_readable[c1$date_readable == enddate-2], 
-                         c1$cases[c1$date_readable == enddate-2], 
-                         label = c1$country[c1$date_readable == enddate-2]),
-                     nudge_y = -10,
+    geom_label_repel(aes(c1$date_readable[c1$date_readable == enddate-1], 
+                         c1$cases[c1$date_readable == enddate-1], 
+                         label = c1$country[c1$date_readable == enddate-1]),
+                     nudge_y = -150,
                      nudge_x = 5) +
     geom_text_repel(aes(c1$date_readable[c1$date_readable == enddate], 
                         c1$cases[c1$date_readable == enddate], 
@@ -76,10 +76,10 @@ for (i in list (1,2,3,4,5)) {
   p <- p +
     geom_line(aes(c2$date_readable, c2$cases),col = "blue", size = 1) +
     geom_point(aes(c2$date_readable, c2$cases),col = "black", size = 2) +
-    geom_label_repel(aes(c2$date_readable[c2$date_readable == enddate-2], 
-                         c2$cases[c2$date_readable == enddate-2], 
-                         label = c2$country[c2$date_readable == enddate-2]),
-                     nudge_y = -10,
+    geom_label_repel(aes(c2$date_readable[c2$date_readable == enddate-1], 
+                         c2$cases[c2$date_readable == enddate-1], 
+                         label = c2$country[c2$date_readable == enddate-1]),
+                     nudge_y = -75,
                      nudge_x = 5) +
     geom_text_repel(aes(c2$date_readable[c2$date_readable == enddate], 
                         c2$cases[c2$date_readable == enddate], 
@@ -96,10 +96,10 @@ for (i in list (1,2,3,4,5)) {
   p <- p +
     geom_line(aes(c3$date_readable, c3$cases),col = "orange", size = 1) +
     geom_point(aes(c3$date_readable, c3$cases),col = "black", size = 2) +
-    geom_label_repel(aes(c3$date_readable[c3$date_readable == enddate-2], 
-                         c3$cases[c3$date_readable == enddate-2], 
-                         label = c3$country[c3$date_readable == enddate-2]),
-                     nudge_y = -10,
+    geom_label_repel(aes(c3$date_readable[c3$date_readable == enddate-1], 
+                         c3$cases[c3$date_readable == enddate-1], 
+                         label = c3$country[c3$date_readable == enddate-1]),
+                     nudge_y = 0,
                      nudge_x = 5) +
     geom_text_repel(aes(c3$date_readable[c3$date_readable == enddate], 
                         c3$cases[c3$date_readable == enddate], 
